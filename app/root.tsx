3541b6e059cd805d076a346f394eab192fce68a7
Setyo <setyo.nugroho@ppatk.go.id>
2024-06-05 16:27:11 +0700

    Wed Jun  5 04:27:11 PM WIB 2024

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,31 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import './index.css'
+import { ApplicationLayout } from './application-layout'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
+    >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link rel="icon" href="/teams/catalyst.svg" type="image/svg+xml"></link>
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <ApplicationLayout>{children}</ApplicationLayout>
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,368 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
+import { Badge } from '@/catalyst/badge'
+import { Divider } from '@/catalyst/divider'
+import { Heading } from '@/catalyst/heading'
+import { Select } from '@/catalyst/select'
+
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
+import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid'
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
+
+const statuses = {
+  High: 'text-green-700 bg-green-50 ring-green-600/20',
+  Withdraw: 'text-gray-600 bg-gray-50 ring-gray-500/10',
+  Low: 'text-red-700 bg-red-50 ring-red-600/10',
+}
+const clients = [
+  {
+    id: 1,
+    name: 'Tuple',
+    imageUrl: 'https://tailwindui.com/img/logos/48x48/tuple.svg',
+    lastInvoice: {
+      date: 'December 13, 2022',
+      dateTime: '2022-12-13',
+      amount: '92%',
+      status: 'High',
+    },
+  },
+  {
+    id: 2,
+    name: 'SavvyCal',
+    imageUrl: 'https://tailwindui.com/img/logos/48x48/savvycal.svg',
+    lastInvoice: {
+      date: 'January 22, 2023',
+      dateTime: '2023-01-22',
+      amount: '95%',
+      status: 'High',
+    },
+  },
+  {
+    id: 3,
+    name: 'Reform',
+    imageUrl: 'https://tailwindui.com/img/logos/48x48/reform.svg',
+    lastInvoice: {
+      date: 'January 23, 2023',
+      dateTime: '2023-01-23',
+      amount: '76%',
+      status: 'Low',
+    },
+  },
+]
+
+const classNames = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'Fusion Track Client App' },
+    { name: 'description', content: 'Welcome to Fusion Track!' },
+  ]
+}
 
-export default function Index() {
+export function Clients() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+    <ul className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+      {clients.map((client) => (
+        <li key={client.id} className="overflow-hidden rounded-xl border border-gray-200">
+          <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
+            <img
+              src={client.imageUrl}
+              alt={client.name}
+              className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
+            />
+            <div className="text-sm font-medium leading-6 text-gray-900">{client.name}</div>
+            <Menu as="div" className="relative ml-auto">
+              <MenuButton className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500">
+                <span className="sr-only">Open options</span>
+                <EllipsisHorizontalIcon className="h-5 w-5" aria-hidden="true" />
+              </MenuButton>
+              <Transition
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <MenuItems className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                  <MenuItem>
+                    {({ focus }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          focus ? 'bg-gray-50' : '',
+                          'block px-3 py-1 text-sm leading-6 text-gray-900'
+                        )}
+                      >
+                        View<span className="sr-only">, {client.name}</span>
+                      </a>
+                    )}
+                  </MenuItem>
+                  <MenuItem>
+                    {({ focus }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          focus ? 'bg-gray-50' : '',
+                          'block px-3 py-1 text-sm leading-6 text-gray-900'
+                        )}
+                      >
+                        Edit<span className="sr-only">, {client.name}</span>
+                      </a>
+                    )}
+                  </MenuItem>
+                </MenuItems>
+              </Transition>
+            </Menu>
+          </div>
+          <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+            <div className="flex justify-between gap-x-4 py-3">
+              <dt className="text-gray-500">Last synchronization</dt>
+              <dd className="text-gray-700">
+                <time dateTime={client.lastInvoice.dateTime}>{client.lastInvoice.date}</time>
+              </dd>
+            </div>
+            <div className="flex justify-between gap-x-4 py-3">
+              <dt className="text-gray-500">Model Version</dt>
+              <dd className="text-gray-700">
+                <time dateTime={client.lastInvoice.dateTime}>A1.0</time>
+              </dd>
+            </div>
+            <div className="flex justify-between gap-x-4 py-3">
+              <dt className="text-gray-500">Accuracy</dt>
+              <dd className="flex items-start gap-x-2">
+                <div className="font-medium text-gray-900">{client.lastInvoice.amount}</div>
+                <div
+                  className={classNames(
+                    statuses[client.lastInvoice.status],
+                    'rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset'
+                  )}
+                >
+                  {client.lastInvoice.status}
+                </div>
+              </dd>
+            </div>
+          </dl>
         </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      ))}
+    </ul>
+  )
+}
+
+export function Stat({ title, value, change }: { title: string; value: string; change: string }) {
+  return (
+    <div>
+      <Divider />
+      <div className="mt-6 text-lg/6 font-medium sm:text-sm/6">{title}</div>
+      <div className="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">{value}</div>
+      <div className="mt-3 text-sm/6 sm:text-xs/6">
+        <Badge color={change.startsWith('+') ? 'lime' : 'pink'}>{change}</Badge>{' '}
+        <span className="text-zinc-500">from last week</span>
+      </div>
     </div>
-  );
+  )
+}
+
+const plans = [
+  {
+    id: 1,
+    name: 'Hobby',
+    memory: '4 GB RAM',
+    cpu: '4 CPUs',
+    storage: '128 GB SSD disk',
+    price: '$40',
+    isCurrent: false,
+  },
+  {
+    id: 2,
+    name: 'Startup',
+    memory: '8 GB RAM',
+    cpu: '6 CPUs',
+    storage: '256 GB SSD disk',
+    price: '$80',
+    isCurrent: true,
+  },
+  // More plans...
+]
+
+export function Table() {
+  return (
+    <div className="">
+      <div className="-mx-4 mt-10 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg">
+        <table className="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+              >
+                Fraud ID
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+              >
+                Date
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+              >
+                Sender FI
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+              >
+                Receiver FI
+              </th>
+              <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                Customer Name
+              </th>
+              <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                NIK
+              </th>
+              <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                Transaction Amount
+              </th>
+              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                <span className="sr-only">Select</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {plans.map((plan, planIdx) => (
+              <tr key={plan.id}>
+                <td
+                  className={classNames(
+                    planIdx === 0 ? '' : 'border-t border-transparent',
+                    'relative py-4 pl-4 pr-3 text-sm sm:pl-6'
+                  )}
+                >
+                  <div className="font-medium text-gray-900">
+                    {plan.name}
+                    {plan.isCurrent ? (
+                      <span className="ml-1 text-indigo-600">(Current Plan)</span>
+                    ) : null}
+                  </div>
+                  <div className="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
+                    <span>
+                      {plan.memory} / {plan.cpu}
+                    </span>
+                    <span className="hidden sm:inline">·</span>
+                    <span>{plan.storage}</span>
+                  </div>
+                  {planIdx !== 0 ? (
+                    <div className="absolute -top-px left-6 right-0 h-px bg-gray-200" />
+                  ) : null}
+                </td>
+                <td
+                  className={classNames(
+                    planIdx === 0 ? '' : 'border-t border-gray-200',
+                    'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
+                  )}
+                >
+                  {plan.memory}
+                </td>
+                <td
+                  className={classNames(
+                    planIdx === 0 ? '' : 'border-t border-gray-200',
+                    'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
+                  )}
+                >
+                  {plan.cpu}
+                </td>
+                <td
+                  className={classNames(
+                    planIdx === 0 ? '' : 'border-t border-gray-200',
+                    'hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell'
+                  )}
+                >
+                  {plan.storage}
+                </td>
+                <td
+                  className={classNames(
+                    planIdx === 0 ? '' : 'border-t border-gray-200',
+                    'px-3 py-3.5 text-sm text-gray-500'
+                  )}
+                >
+                  <div className="sm:hidden">{plan.price}/mo</div>
+                  <div className="hidden sm:block">{plan.price}/month</div>
+                </td>
+                <td
+                  className={classNames(
+                    planIdx === 0 ? '' : 'border-t border-transparent',
+                    'relative py-3.5 pl-3 pr-4 text-right text-sm font-medium sm:pr-6'
+                  )}
+                >
+                  <button
+                    type="button"
+                    className="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
+                    disabled={plan.isCurrent}
+                  >
+                    Select<span className="sr-only">, {plan.name}</span>
+                  </button>
+                  {planIdx !== 0 ? (
+                    <div className="absolute -top-px left-0 right-6 h-px bg-gray-200" />
+                  ) : null}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default function Home() {
+  return (
+    <>
+      <Heading>Good afternoon, Jessica</Heading>
+
+      <div className="mt-8 flex items-end justify-between">
+        <div className="flex items-center justify-between mt-8 [--gutter:theme(spacing.6)] lg:[--gutter:theme(spacing.10)]">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Overview</h2>
+        </div>
+        <div>
+          <Select name="period">
+            <option value="last_week">Last week</option>
+            <option value="last_two">Last two weeks</option>
+            <option value="last_month">Last month</option>
+            <option value="last_quarter">Last quarter</option>
+          </Select>
+        </div>
+      </div>
+      <div className="mt-4 grid gap-8 sm:grid-cols-2 xl:grid-cols-4">
+        <Stat title="Accuracy" value="95%" change="+4.5%" />
+        <Stat title="Precision" value="90%" change="-0.5%" />
+        <Stat title="Recall" value="88%" change="+4.5%" />
+        <Stat title="Alert Count" value="823,067" change="+21.2%" />
+      </div>
+      <div className="flex items-center justify-between mt-8 [--gutter:theme(spacing.6)] lg:[--gutter:theme(spacing.10)]">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">Recent clients</h2>
+        <a
+          href="/"
+          className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+        >
+          View all<span className="sr-only">, clients</span>
+        </a>
+      </div>
+      <div className="mt-4 [--gutter:theme(spacing.6)] lg:[--gutter:theme(spacing.10)]">
+        <Clients />
+      </div>
+      <div className="flex items-center justify-between mt-8 [--gutter:theme(spacing.6)] lg:[--gutter:theme(spacing.10)]">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">
+          Fraud Detection in BI-Fast Transactions
+        </h2>
+        <div className="mt-4 sm:ml-auto sm:mt-0 sm:flex-none">
+          <button className="flex items-center gap-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <ArrowPathIcon className="h-6 w-6" />
+          </button>
+        </div>
+      </div>
+      <div className="mt-4 [--gutter:theme(spacing.6)] lg:[--gutter:theme(spacing.10)]">
+        <Table />
+      </div>
+    </>
+  )
 }
